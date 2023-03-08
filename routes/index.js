10bb1b7735fb80c3b150b78204264f3858adb678
Klara Gicheva <klara.gicheva@student.um.si>
2023-03-08 17:24:46 +0100

    Za pocetok ...

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/funkcionalnostiOdjemalca', function(req, res, next) {
+  res.render('client', { client: 'Funkcionalnosti Odjemalca' });
+});
+
+router.get('/posebnosti', function(req, res, next) {
+  res.render('features', { features: 'Posebnosti' });
+});
+
+router.get('/podatkovniModel', function(req, res, next) {
+  res.render('database', { database: 'Podatkovni Model' });
+});
+
+router.get('/rest', function(req, res, next) {
+  res.render('rest', { rest: 'REST' });
+});
+
 module.exports = router;
